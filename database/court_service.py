ad2cc97a46e345a657dfd29a3cef7de23b8a5d23
tomsalescamargo <tomsalescama@gmail.com>
2025-10-12 19:20:51 -0300

    courts CRUD

--- database/court_service.py
+++ database/court_service.py
@@ -18,3 +18,30 @@ class CourtService(SupabaseClient):
         }
         response = self._client.table('courts').insert(court_dict).execute()
         return response
+
+    def update_court(self, court: Court):
+        court_dict = {
+            "name": court.name,
+            "court_type": court.court_type,
+            "description": court.description,
+            "capacity": court.capacity,
+            "price_per_hour": court.price_per_hour,
+            "start_hour": court.start_hour,
+            "end_hour": court.end_hour,
+        }
+        response = (
+            self._client.table('courts')
+            .update(court_dict)
+            .eq('id', court.id)
+            .execute()
+        )
+        return response
+
+    def delete_court(self, court_id: int):
+        response = (
+            self._client.table('courts')
+            .delete()
+            .eq('id', court_id)
+            .execute()
+        )
+        return response
